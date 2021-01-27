fba5c4fe37198a8c48b68d1e37bdfd70b718e34c
ShoRaz~ <shorajmanandhar@hotmail.com>
2021-01-27 15:56:13 -0800

    Test github - Shoraj

---
+++ app/src/main/java/com/example/warehousemanagmentsystem491b/Login/Login.java
@@ -0,0 +1,18 @@
+package com.example.warehousemanagmentsystem491b.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.warehousemanagmentsystem491b.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
--- app/src/main/java/com/example/warehousemanagmentsystem491b/MainActivity.java
+++ app/src/main/java/com/example/warehousemanagmentsystem491b/MainActivity.java
@@ -22,4 +22,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
+
 }
